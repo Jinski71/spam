9996d98bf99e5df91a9a44e3498d8254f60038fa
sean.desktop <saemaromoon@zigbang.com>
2022-01-18 17:54:30 +0900

    add

--- s3.py
+++ s3.py
@@ -120,7 +120,8 @@ def load_trained_model():
 
 @timer
 def inference(model, data):
-    round(model.predict(data)[0] * 100, 2)
+    prob = round(model.predict(data)[0][0] * 100, 2)
+    return prob
 
 @timer
 def main():
